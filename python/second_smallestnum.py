8f60622fb6cbcaee3640f97f9544fe5ccf5e3fa6
Hasini Varma <hasinivarma16@gmail.com>
2026-01-18 22:00:00 +0530

    added

--- python/second_smallestnum.py
+++ python/second_smallestnum.py
@@ -5,6 +5,6 @@ for i in arr:
     if i<first_smallest: #should be less
         second_smallest=first_smallest #equals
         first_smallest=i
-    elif i<second_smallest and i!=first_smallest:
+    elif i<second_smallest and i!=first_smallest: #else
         second_smallest=i
 print(second_smallest)
