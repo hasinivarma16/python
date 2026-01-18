7b29cbaceec3fa3cc41c3635319ec34297354873
Hasini Varma <hasinivarma16@gmail.com>
2026-01-18 22:00:00 +0530

    added

--- python/second_smallestnum.py
+++ python/second_smallestnum.py
@@ -3,7 +3,7 @@ first_smallest=float('inf')
 second_smallest=float('inf')
 for i in arr:
     if i<first_smallest: #should be less
-        second_smallest=first_smallest
+        second_smallest=first_smallest #equals
         first_smallest=i
     elif i<second_smallest and i!=first_smallest:
         second_smallest=i
