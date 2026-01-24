3290559458be66154e35817b2209d56019cab178
Hasini Varma <hasinivarma16@gmail.com>
2026-01-24 22:00:00 +0530

    added

---
+++ largestnum_k.py
@@ -0,0 +1,16 @@
+arr=[50,30,23,12,9,21]
+k=int(input())
+first_lar=float('-inf')
+second_lar=float('-inf')
+third_lar=float('-inf')
+for num in arr:
+    if num > first_lar:
+        third_lar = second_lar
+        second_lar = first_lar
+        first_lar = num
+    elif num > second_lar:
+        third_lar = second_lar
+        second_lar = num
+    elif num > third_lar:
+        third_lar = num
+print(third_lar)
