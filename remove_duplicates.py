fe514e384f67fc40834744e101e9d5f993f4e3bd
Hasini Varma <hasinivarma16@gmail.com>
2026-01-26 22:00:00 +0530

    added

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,9 +1,9 @@
 #remove duplicates from a list using two pointers
 num=[1,1,2,2,3]
 i=0
-j=i+1
+j=i+1 
 for i in range(len(num)-1):
     for j in range(i+1,len(num)-1):
         if num[i]==num[j]:
-            num.remove(j)
+            num.remove(j)#remove duplicates
 print(num) #print or return nums
