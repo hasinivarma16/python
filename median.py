9eb845a7976fd3f8dda07ae2a057c6af6b49c40c
Hasini Varma <hasinivarma16@gmail.com>
2026-02-01 22:00:00 +0530

    added

--- median.py
+++ median.py
@@ -2,7 +2,7 @@
 arr=[2,3,4,7,8,9]
 len_arr=len(arr)
 if len_arr%2==0:
-    median=(arr[len_arr//2]+arr[(len_arr//2)-1])/2
+    median=(arr[len_arr//2]+arr[(len_arr//2)-1])/2 #divide
 else:
     median=arr[len_arr//2]#print median
 print(median)
