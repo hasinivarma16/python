0c46cd77559c1208497ef272804ad9d3ac631ebd
Hasini Varma <hasinivarma16@gmail.com>
2026-02-09 22:00:00 +0530

    added

--- add_even_odd.py
+++ add_even_odd.py
@@ -1,5 +1,14 @@
 arr=[2,7,8,11,3]
 even=[]
-odd=[]
+odd=[]  
 sum_even=0
 sum_odd=0
+for i in arr:
+    if i%2==0:
+        sum_even+=i
+        
+    if i%2!=0:
+        sum_odd+=i
+even.append(sum_even)
+odd.append(sum_odd)
+print(even+odd)
