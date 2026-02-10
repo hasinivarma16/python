c494a16e50dbdccfe64a691fcd8215e1c88fc733
Hasini Varma <hasinivarma16@gmail.com>
2026-02-10 22:00:00 +0530

    added

--- zeros_at_last.py
+++ zeros_at_last.py
@@ -4,7 +4,7 @@ first_arr=[]#list1
 second_arr=[]#list2
 for i in arr:
     if i==0:
-        second_arr.append(i)
+        second_arr.append(i) #if 0
     else:
         first_arr.append(i)
 print(first_arr+second_arr)
