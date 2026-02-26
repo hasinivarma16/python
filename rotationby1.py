ee6b01d4760d298c062dcda4a4b8f44abf5cf7af
Hasini Varma <hasinivarma16@gmail.com>
2026-02-26 11:00:10 +0530

    added

--- rotationby1.py
+++ rotationby1.py
@@ -1,3 +1,12 @@
-a=[1,2,3,4,5]
+
+'''a=[1,2,3,4,5]
 a=[a[-1]]+a[:-1]
-print(a)
+print(a)'''
+
+num = [1, 2, 3, 4, 5]
+temp = num[0]
+for i in range(len(num) - 1):
+    num[i] = num[i + 1]
+num[len(num) - 1] = temp
+
+print(num)
