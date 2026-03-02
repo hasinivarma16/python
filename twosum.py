f284819f3d7481e9c65bb31e42ead8d3e4fb7e57
Hasini Varma <hasinivarma16@gmail.com>
2026-03-02 18:57:25 +0530

    added

--- twosum.py
+++ twosum.py
@@ -3,6 +3,5 @@ target=5
 for i in range(len(arr)):
     for j in range(0,len(arr)):
         if arr[i]+arr[j]==target:
-            print(i,j)
-
-        
+            print(i,j) #printed
+            
