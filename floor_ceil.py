2c4991df88d836a2cc2ded2b8b7f51f4c1bf0a4f
Hasini Varma <hasinivarma16@gmail.com>
2026-03-03 10:26:46 +0530

    added

---
+++ floor_ceil.py
@@ -0,0 +1,19 @@
+arr=[10,20,30,40,50]
+target=25
+left=0 #low
+right=len(arr)-1 #high
+floor=-1
+ceil=-1
+while left<=right:
+    mid=(left+right)//2   #becoz for every loop mid changes/updates
+    if arr[mid]==target:
+        floor=arr[mid]
+        ceil=arr[mid]
+    if arr[mid]<=target:
+        floor = arr[mid]      
+        left = mid + 1  
+    if arr[mid]>=target:
+        ceil = arr[mid]      
+        right = mid - 1
+print("floor:",floor)
+print("ceil:",ceil)
