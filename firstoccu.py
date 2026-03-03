8e034a958c35ce90c06e8511e43d358c40df08ed
Hasini Varma <hasinivarma16@gmail.com>
2026-03-03 10:34:39 +0530

    added

---
+++ firstoccu.py
@@ -0,0 +1,15 @@
+arr=[3,4,13,13,13,20,40]
+target=13
+left=0
+right=len(arr)-1
+ans=-1  #not found, so it stores element
+while left<=right:
+    mid=(left+right)//2   #becoz for every loop mid changes/updates
+    if arr[mid]<target:
+        left=mid+1
+    elif arr[mid]>target:
+        right=mid-1
+    elif arr[mid]==target:
+        ans=mid
+        right=mid-1    #ele on left side so -1 ,#for last occurrence right=mid+1
+print("first occ:",ans)
